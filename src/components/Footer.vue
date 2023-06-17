39d385c85cb41f56b4e6e12ec772e17afc23e8a4
ocean_520x <ocean_520x@hchow.icu>
2023-06-17 21:46:42 +0800

    feat:实现页面右侧上中下布局

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="footer">
+  <div class="left">所有权归ocean所有</div>
+    <div class="right">
+      <a target="_blank" href="https://github.com/ocean520x/newBee">新蜂商城在线仓库地址</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  height: 50px;
+  border-top: 1px solid #e9e9e9;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  a {
+    text-decoration: none;
+    color: #1baeae;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from "@/router/index.js";
-import { ElButton } from 'element-plus'
+import {ElButton, ElContainer, ElAside, ElMenu, ElSubMenu, ElMenuItemGroup,ElMenuItem} from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
-app.use(router).use(ElButton).mount('#app')
+app.use(router).use(ElButton).use(ElContainer).use(ElAside).use(ElMenu).use(ElSubMenu).use(ElMenuItemGroup).use(ElMenuItem).mount('#app')
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
